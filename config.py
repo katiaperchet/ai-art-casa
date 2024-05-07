13f41fa51581ea8aa80e7d552f77b9cda1303ed7
Katia Perchet <53318951+katiaperchet@users.noreply.github.com>
2024-05-07 17:46:27 +0200

    Renamed main file and set it as configuration file for paths

---
+++ config.py
@@ -0,0 +1,4 @@
+# Configuration file to set the path of the datasets for each user
+
+TRAIN_DATA_DIR = 'datasets/training_data/train' #Change it for you path if necessary
+VALID_DATA_DIR = 'datasets/training_data/valid'  #Change it for you path if necessary
