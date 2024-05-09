81327b1f296465076540f377b954cae1c6b1bc94
merendafrancon <merenda.franco83@gmail.com>
2024-05-09 05:11:43 -0300

    Added flags to run in remote server

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 # Set up LD_LIBRARY_PATH for CUDA
 export LD_LIBRARY_PATH=/home/millenium-falcon/.local/lib/python3.10/site-packages/nvidia/cuda_nvrtc/lib:/home/millenium-falcon/.local/lib/python3.10/site-packages/nvidia/cublas/lib:/home/millenium-falcon/.local/lib/python3.10/site-packages/nvidia/cudnn/lib
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/local/cuda
 jupyter-notebook
