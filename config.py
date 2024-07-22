d87a6e513c3f3709032ba86489f36c98c8a12d6f
y3rbiadit0 <yerbiadito@gmail.com>
2024-07-22 06:51:54 +0200

    Removed unused config file

--- config.py
+++
@@ -1,4 +0,0 @@
-# Configuration file to set the path of the datasets for each user
-
-TRAIN_DATA_DIR = 'datasets/training_data/train' #Change it for you path if necessary
-VALID_DATA_DIR = 'datasets/training_data/valid'  #Change it for you path if necessary
